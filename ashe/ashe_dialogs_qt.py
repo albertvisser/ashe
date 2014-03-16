16c3fc46228f46397f8511eda74373e566921759
Albert Visser <albert@lemoncurry>
2014-03-16 23:29:10 +0100

    409 idem voor links in de header

--- ashe/ashe_dialogs_qt.py
+++ ashe/ashe_dialogs_qt.py
@@ -368,9 +368,10 @@ class CssDialog(gui.QDialog):
         "bij OK: het geselecteerde (absolute) pad omzetten in een relatief pad"
         link = str(self.link_text.text())
         if link:
-            if not link.startswith('http://') and not link.startswith('/'):
+            if not link.startswith('http://'):
+                link = os.path.abspath(link)
                 if self._parent.xmlfn:
-                    whereami = self._parent.xmlfn
+                    whereami = os.path.abspath(self._parent.xmlfn)
                 else:
                     whereami = os.path.join(os.getcwd(),'index.html')
                 link = ed.getrelativepath(link, whereami)
