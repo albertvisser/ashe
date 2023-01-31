d7cb8f793679b592aa8c577cf8c47feda96a92e6
Albert Visser <albert.visser@gmail.com>
2023-01-31 22:05:21 +0100

    placeholder for unittests

---
+++ unittests/test_shared.py
@@ -0,0 +1,10 @@
+import pytest
+import ashe.shared
+
+def _test_analyze_element(monkeypatch, capsys):
+    ...
+
+def _test_get_dtdmenu_texts(monkeypatch, capsys):
+    ...
+    # nog kijken hoe dit in de gui modules gebruikt moet worden om hardgecodeerde teksten te
+    # vervangen
